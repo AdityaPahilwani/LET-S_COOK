24da0dc77c915a7aef16d210a91012f66e787e23
Insomaniacgamer <aditya.pahilwani@gmail.com>
2020-06-01 16:00:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 
   var firebaseConfig = {
-    apiKey: "AIzaSyD682v8FH_hBEtPYAM7hHrja-5xHa1hF44",
-    authDomain: "recipe-292c7.firebaseapp.com",
-    databaseURL: "https://recipe-292c7.firebaseio.com",
-    projectId: "recipe-292c7",
-    storageBucket: "recipe-292c7.appspot.com",
-    messagingSenderId: "802451063029",
-    appId: "1:802451063029:web:51cbae1c36b2c9cf31c5bc",
-    measurementId: "G-YTRVFJJSCV"
+      // paste your web confiq file here
   }
 
   export default firebaseConfig;
